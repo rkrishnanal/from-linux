7aaf2aebf13b946749790ce8be792245786fdab8
rkrishnanal <rkrishnanal.it@gmail.com>
2021-03-10 13:04:50 +0300

    Updated

--- remotesoftware.ps1
+++ remotesoftware.ps1
@@ -1,3 +1,6 @@
+
+#Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value "Computer1,Computer2"#
+
 $remote1=Set-Item WSMan:\localhost\Client\TrustedHosts 10.2.1.213
 $server=Get-Content -Path .\server.txt 
 $credentinals=Get-Credential
@@ -5,7 +8,7 @@ foreach ($servers in $server) {
     Invoke-Command -ComputerName $servers -ScriptBlock {
         $uri="https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.3/npp.7.9.3.Installer.x64.exe"
         $outfile= "C:\Windows\Temp\npp.7.9.3.Installer.x64.exe"
-        Invoke-WebRequest -Uri $uri -Method Get -OutFile $outfile
+        Invoke-WebRequest -u $uri -Method Get -OutFile $outfile
         Start-Process -FilePath $outfile -ArgumentList "/S" -Wait
     } -Credential $credentinals
     
