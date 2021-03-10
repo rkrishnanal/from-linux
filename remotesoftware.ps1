929cce52a6bd097cbbafbbb24d4f18d101bd7b6f
rkrishnanal <rkrishnanal.it@gmail.com>
2021-03-10 12:50:44 +0300

    schooldesktop

---
+++ remotesoftware.ps1
@@ -0,0 +1,12 @@
+$remote1=Set-Item WSMan:\localhost\Client\TrustedHosts 10.2.1.213
+$server=Get-Content -Path .\server.txt 
+$credentinals=Get-Credential
+foreach ($servers in $server) {
+    Invoke-Command -ComputerName $servers -ScriptBlock {
+        $uri="https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.3/npp.7.9.3.Installer.x64.exe"
+        $outfile= "C:\Windows\Temp\npp.7.9.3.Installer.x64.exe"
+        Invoke-WebRequest -Uri $uri -Method Get -OutFile $outfile
+        Start-Process -FilePath $outfile -ArgumentList "/S" -Wait
+    } -Credential $credentinals
+    
+}
