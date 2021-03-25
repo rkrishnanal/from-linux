6d529d9b45d02f9be5453fde6ceafccb405ff1e3
rkrishnanal <rkrishnanal.it@gmail.com>
2021-03-25 15:38:09 +0300

    Interview Details

--- Duplicate.ps1
+++ Duplicate.ps1
@@ -2,4 +2,16 @@ $filepath = Read-Host " Enter the path name"
 $duplicates= Get-ChildItem -Path $filepath |Get-FileHash | Group-Object -Property Hash | Where-Object count -gt 1 
 
 #latency
-repadmin.exe /latency 10.2.1.213
+repadmin.exe /showrepl Error Messages
+
+#list computer
+Get-ADComputer -Filter 'enabled -eq "true"' `
+-Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address |
+Sort-Object -Property Operatingsystem |
+Select-Object -Property Name,Operatingsystem,OperatingSystemVersion,IPv4Address  |ft
+
+#Seriall Number 
+Get-ADComputer -Filter *| ForEach-Object {Get-CimInstance -ClassName Win32_BIOS -ComputerName $_ -ErrorAction SilentlyContinue } |
+Select-Object PSComputerName ,SerialNumber
+
+ 
